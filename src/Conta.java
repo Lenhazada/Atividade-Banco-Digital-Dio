8c0c6f8a82eb4254b4f406b6e44791c8577a5228
Gabriel Lenhardt <gabriellenhardtcontato@gmail.com>
2024-06-04 18:47:46 -0300

    Iniciando repositório

---
+++ src/Conta.java
@@ -0,0 +1,54 @@
+public abstract class Conta implements IConta{
+
+    private static int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+    protected Cliente cliente;
+
+    public Conta(Cliente cliente){
+        this.agencia = Conta.AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+        this.cliente = cliente;
+    }
+
+    //getters
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+    //----------------------------
+
+
+    @Override
+    public void sacar(double valor) {
+        saldo -= valor;
+    }
+
+    @Override
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    @Override
+    public void transferir(double valor, IConta contaDestino) {
+        this.sacar(valor);
+        contaDestino.depositar(valor);
+    }
+
+    protected void imprimirInfosComuns() {
+        System.out.printf("Titular: %s | ", cliente.getNome());
+        System.out.printf("Agencia: %d | ", agencia);
+        System.out.printf("Número: %d | ", numero);
+        System.out.printf("Saldo: %.2f%n ", saldo);
+    }
+}
